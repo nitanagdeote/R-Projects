17545aff046ee856809c05f0fa1d39f126c761fa
nitanagdeote <nitanagdeote1@gmail.com>
2016-06-01 16:08:50 +0530

    added file

---
+++ file1.R
@@ -0,0 +1,12 @@
+
+library(sp)
+library(raster)
+library(rgdal)
+a <- raster("~/Desktop/1_1.tif")
+
+train_masks <- read.csv("~/Downloads/train_masks.csv")
+b <- train_masks$pixels[1]
+
+c <- raster(10,10)
+c[] <- a
+
